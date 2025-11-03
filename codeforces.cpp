0102a1156d16fa028830c264f609af00bcacad4d
Faiz Ahmad <faizahmad43430@gmail.com>
2025-11-03 19:59:10 +0530

    CP11

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-string s;
-unordered_map<long long, long long> memo[20]; // memo[idx][x] = seconds
-
-long long dfs(long long x, int idx) {
-    if (x == 0) return 0;
-    if (memo[idx].count(x)) return memo[idx][x];
-
-    long long next;
-    if (s[idx] == 'A') next = dfs(x - 1, (idx + 1) % n);
-    else next = dfs(x / 2, (idx + 1) % n);
-
-    return memo[idx][x] = 1 + next;
-}
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--) {
-        int q;
-        cin >> n >> q;
-        cin >> s;
-        vector<long long> a(q);
-        for (int i = 0; i < q; i++) cin >> a[i];
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        int maxi = 0, ans = -1;
 
-        // clear memo
-        for (int i = 0; i < n; i++) memo[i].clear();
-
-        for (int i = 0; i < q; i++) {
-            cout << dfs(a[i], 0) << "\n";
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            maxi = max(maxi, a[i]);
         }
+
+
+        if (n == 1 && a[0] == 1) {
+            cout << 2 << endl;
+            continue;
+        }
+
+        for (int x = 2; x < maxi; x++) {
+            bool found = false;
+            for (int i = 0; i < n; i++) {
+                if (__gcd(x, a[i]) == 1) {
+                    ans = x;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+
+        cout << ans << endl;
     }
 }
