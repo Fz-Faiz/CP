9f035659523a35a75355c8fffe7e421c2583a55c
Faiz Ahmad <faizahmad43430@gmail.com>
2026-08-18 01:35:16 +0530

    Advanced dsa started

---
+++ TLE/1100/1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+        map<char,int>freq;
+
+        vector<int> distinct(n, 0);
+        int count = 0;
+
+        for(int i = 0; i < n; ++i){
+            freq[s[i]]++;
+            if(freq[s[i]] == 1){
+                count++;
+            }
+            distinct[i] = count;
+        }
+
+        long long ans = 0;
+        for(int i = 0; i < n; ++i){
+            ans += distinct[i];
+        }
+        cout << ans << endl;
+    }
+}
+
+// TC: O(NLogN)
+// SC: O(N)
