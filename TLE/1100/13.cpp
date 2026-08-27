109e9f01c8fb6b97d8901a1893c81f20d961bd32
Faiz Ahmad <faizahmad43430@gmail.com>
2026-08-28 00:21:51 +0530

    1100-13

---
+++ TLE/1100/13.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for(int i = 0; i < n; ++i) cin >> a[i];
+        ll ans = 0;
+        for(int i = 0; i < n / 2; ++i){
+            ans = __gcd(ans, abs(a[i] - a[n-i-1]));
+        }
+        cout << ans << endl;
+    }
+}
+
+// TC: O(Nlog1e9)
